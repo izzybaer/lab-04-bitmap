9f8c11714e38c32c6f94289fa5063943a7ad6665
Nicu Parente <nicuparente@gmail.com>
2017-06-16 16:10:12 -0700

    Added GreyScal and Invert Transforms

--- bitmap-transformers/index.js
+++ bitmap-transformers/index.js
@@ -5,4 +5,4 @@ const bitmapFileRead  = require('./lib/read-write.js');
 const bitmap = require('./lib/bitmap.js');
 const filepath = process.argv[2];
 
-let buff = bitmapFileRead.read(filepath, colorScale.scale);
+let buff = bitmapFileRead.read(filepath, colorScale.greyScale);
--- bitmap-transformers/lib/transform.js
+++ bitmap-transformers/lib/transform.js
@@ -11,3 +11,21 @@ transform.scale = (colorTable) => {
   }
   return colorTable;
 };
+
+transform.greyScale = (colorTable) => {
+  let greyScale;
+  for (var i = 0; i < colorTable.length; i+= 4) {
+    greyScale = (colorTable[i] + colorTable[i+1] + colorTable[i+2]) / 3;
+    colorTable[i] = colorTable[i+1] = colorTable[i+2] = greyScale;
+  }
+  return colorTable;
+};
+
+transform.invert = (colorTable) => {
+  for (var i = 0; i < colorTable.length; i+= 4){
+    colorTable[i] = (255-colorTable[i]);
+    colorTable[i+1] = (255-colorTable[i+1]);
+    colorTable[i+2] = (255-colorTable[i+2]);
+  }
+  return colorTable;
+};
