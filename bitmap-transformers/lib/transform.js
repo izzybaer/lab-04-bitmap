c011f2703e0ce5d54e3269f7d9a52fe820e1ab47
Nicu Parente <nicuparente@gmail.com>
2017-06-16 15:59:26 -0700

    Transform Scale

--- bitmap-transformers/lib/transform.js
+++ bitmap-transformers/lib/transform.js
@@ -4,10 +4,10 @@ const transform = module.exports = {};
 
 transform.scale = (colorTable) => {
   for (var i = 0; i < colorTable.length; i+= 4) {
-    colorTable[i] = 255;
-    colorTable[i+1] = 0;
-    colorTable[i+2] = 0;
-    colorTable[i+3] = 0;
+    //colorTable[i] = 0; //Blue
+    //colorTable[i+1] = 0; //Green
+    colorTable[i+2] = 255; //Red
+    //colorTable[i+3] = 0; // Throw
   }
   return colorTable;
 };
